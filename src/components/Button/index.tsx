2e45287d8b43f5f9ad14dd2bb86776f12cc7b370
leafartech <119262053+leafartech@users.noreply.github.com>
2023-11-13 22:54:50 -0300

    Add files via upload

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+
+interface ButtonProps {
+    children: ReactNode
+    className: string
+}
+
+export default function Button({ children, className }: ButtonProps) {
+    return (
+        <div className="w-full max-w-sm flex flex-col text-center">
+            <a href="#forms" className={`w-full py-3 sm:py-4 text-sm sm:text-base uppercase text-center rounded-xl sm:rounded-2xl text-white font-bold ${className}`}>{children}</a>
+        </div>
+    )
+}
